2674312f41e3349814376585a8c11b657d09863a
fgurbano23 <fgurbano23@gmail.com>
2019-10-06 14:38:43 -0400

    modal de invitaciones pendientes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,6 +8,8 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Modal,
+  TouchableHighlight,
   ListView,
   View,
   ImageBackground,
@@ -19,20 +21,28 @@ import TabBarIcon from "../components/TabBarIcon";
 
 class HomeScreen extends Component{
   state = {
+      'invitationModal':false,
+      'giftModal':false,
     'appData':{
-      'appTotal':5000
+      'appTotal':5000,
     },
     'userInfo':{
       'username':'MyUser',
       'userPosition':123,
     }
   }
+    toggleInvitationModal(visible) {
+        this.setState({ invitationModal: visible });
+    };
+    toggleGiftModal(visible) {
+        this.setState({ giftModal: visible });
+    };
 
   render(){
     return (
       <View style={styles.container}>
-        <ScrollView>
 
+        <ScrollView>
           <View style={ styles.notificationBar}>
               <View style={{flexDirection:'row'}}>
                   <Image
@@ -41,9 +51,11 @@ class HomeScreen extends Component{
                   <Text style={{color:'white',marginLeft:10,fontWeight:'bold'}}>{this.state.userInfo.username}</Text>
               </View>
               <View  style={{flexDirection:'row'}}>
-                  <Image
-                      style={{width: 25, height: 25,  marginRight:15}}
-                      source={require('../assets/images/notification.png')}/>
+                  <TouchableHighlight onPress = {() => {this.toggleInvitationModal(true)}}>
+                      <Image
+                          style={{width: 25, height: 25,  marginRight:15}}
+                          source={require('../assets/images/notification.png')}/>
+                  </TouchableHighlight>
                   <Image
                       style={{width: 25, height: 25, marginRight:10}}
                       source={require('../assets/images/gift_notification.png')}/>
@@ -93,8 +105,54 @@ class HomeScreen extends Component{
             source={require('../assets/images/books_bg.jpg')}/>
             <Button title ="Jugar" color="#EB5757"></Button>
           </View>
-
         </ScrollView>
+
+          {/*NOTIFICATION MODAL*/}
+          <Modal
+              animationType = {"slide"} transparent = {false}
+              visible = {this.state.invitationModal}
+              onRequestClose = {() => { console.log("Modal has been closed.") } }>
+              <View>
+                  <ImageBackground
+                      style={{width: '100%', height: 50,marginTop:30,marginBottom:30}}
+                      source={require('../assets/images/arrow_title.png')}>
+                      <Text style={{textAlign: 'center',color:'white',fontWeight:'bold',fontSize:25}}>Invitaciones</Text>
+                  </ImageBackground>
+                  <View style={{height:'70%'}}>
+                      <ScrollView>
+                          <View style={styles.invitationItem}>
+                              <Image
+                                  style={{width: 35, height: 35}}
+                                  source={require('../assets/images/Ellipse.png')}/>
+                              <Text style={{fontWeight:'bold'}}>{this.state.userInfo.username}</Text>
+                              <View>
+                                  <Button
+                                      title ="Rechazar"
+                                      color="#E0E0E0">
+                                  </Button>
+                              </View>
+                              <View>
+                                  <Button
+                                      title ="Aceptar"
+                                      color="#EB5757">
+                                  </Button>
+                              </View>
+                          </View>
+                      </ScrollView>
+                  </View>
+                  <View style={{position:'absolute',bottom:0,right:30}}>
+                      <Button
+                          title ="Regresar"
+                          color="#EB5757"
+                          onPress={() => this.toggleInvitationModal(!this.state.invitationModal)}>
+                      </Button>
+                  </View>
+              </View>
+
+          </Modal>
+
+          {/*GIFT MODAL */}
+
       </View>
     )
   }
@@ -159,5 +217,16 @@ notificationBar:
         padding:10,
         marginTop:30,
         marginBottom:15
-    }
+    },
+invitationItem:{
+    display:'flex',
+    backgroundColor:'white',
+    flexShrink:0,
+    flexBasis:1,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    padding:5,
+    height:50,
+    marginBottom:15,
+}
 });
